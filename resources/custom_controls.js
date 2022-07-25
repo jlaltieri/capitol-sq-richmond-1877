d45a18ee1ec2d8d90383842345e6b0d5d1a5c105
Jim Altieri <59482188+jlaltieri@users.noreply.github.com>
2022-07-25 11:19:32 -0400

    Update custom_controls.js

--- resources/custom_controls.js
+++ resources/custom_controls.js
@@ -39,3 +39,22 @@ function getLabelDiv(lbl_caption, attr_type, attr_val){
   label_div.setAttribute(attr_type, attr_val);
   return label_div;
 }
+// Shows the slider as having white background for area moved
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+const numberInput = document.querySelector('input[type="number"]')
+
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range')
+  } 
+  const min = target.min
+  const max = target.max
+  const val = target.value
+  
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+  input.addEventListener('input', handleInputChange)
+})
